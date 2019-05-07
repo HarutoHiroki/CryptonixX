f20b25cf04d6ab17617f31ebcd56fef90ed22b64
HarutoHiroki <47408169+HarutoHiroki@users.noreply.github.com>
2019-05-07 08:50:24 +0700

    main files

---
+++ index.js
@@ -0,0 +1,141 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const DBL = require("dblapi.js");
+const settings = require('./settings.json');
+const dbl = new DBL(settings.dblapitoken, client);
+const chalk = require('chalk');
+const fs = require('fs');
+const moment = require('moment');
+//const mysql = require('mysql');
+//const file = require('./mysql.json');
+const mongoose = require('mongoose')
+const activities = require('./data/activities.json');
+db = require('quick.db');
+require('./util/eventLoader')(client);
+
+//loading messages
+const log = message => {
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message}`);
+};
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir('./commands/', (err, files) => {
+  if (err) console.error(err);
+  log(`Loading a total of ${files.length} commands.`);
+  files.forEach(f => {
+    let props = require(`./commands/${f}`);
+    log(`Command Loaded! ${props.help.name} 👌`);
+    client.commands.set(props.help.name, props);
+    props.conf.aliases.forEach(alias => {
+      client.aliases.set(alias, props.help.name);
+    });
+  });
+});
+
+client.on('ready', () => {
+  setInterval(() => {
+    dbl.postStats(client.guilds.size);
+    //client.user.setPresence({ game: { name: `${activities[Math.floor(Math.random() * activities.length)]}`, type: 1, url: "https://www.twitch.tv/hiroaki_haruto" }})
+  }, 60000);
+});
+
+client.on("guildCreate", guild => {
+  let channelID;
+  let channels = guild.channels;
+  channelLoop:
+  for (let c of channels) {
+      let channelType = c[1].type;
+      if (channelType === "text") {
+          channelID = c[0];
+          break channelLoop;
+      }
+  }
+
+  let channel = client.channels.get(guild.systemChannelID || channelID);
+  channel.send(`Thanks for inviting me into this server! Please do /info and /help for the informations you WILL need in order for the bot to work properly. Do /suggest or /bug if there's any suggestions or bug you found. THANKS`);
+  channel.send("Join me in the Developer's server https://discord.gg/VCdNdnc");
+
+  let blacklist = JSON.parse(fs.readFileSync("./blacklist.json", "utf8"));
+    client.guilds.forEach((guild) => {
+      if (!blacklist[guild.ownerID]) return
+      if(blacklist[guild.ownerID].state === true) {
+        message.channel.send("But UNFORTUNATELY, the owner of this server has been blacklisted before so I'm LEAVING! Bye!")
+        message.guild.leave(guild.id)
+      }
+    })
+});
+//command reload
+client.reload = command => {
+  //if(message.author.id !=="242263403001937920") return message.channel.send(`**»** ${message.author}, you don't have permission to do that❌`);
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./commands/${command}`)];
+      let cmd = require(`./commands/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e){
+      reject(e);
+    }
+  });
+};
+
+//connect to MySQL database
+//var con = mysql.createConnection({
+//  host: file.SQL_host,
+//  user: file.SQL_user,
+//  password: file.SQL_password,
+//  database: file.SQL_database
+//
+//});
+//
+//con.connect(err => {
+//  if(err) throw err;
+//  console.log(chalk.bgGreen.black("Connected to MySQL database!"));
+//  
+//});
+
+mongoose.connect('mongodb://localhost:27017/DiscordDB', { useNewUrlParser: true }, err => {
+    if (err) return console.error(err);
+    console.log(chalk.bgGreen.black('Connected to MongoDB database!'));
+});
+
+  client.on('guildMemberAdd', async (member) => {
+    let autoRole = await db.fetch(`autorole_${member.guild.id}`);
+    if (!autoRole) return;
+    if (autoRole === 'none') return;
+    let autorole = member.guild.roles.find('name', autoRole);
+    member.addRole(autorole);
+});
+
+//client command elevation(permLevel)
+client.elevation = message => {
+  if (message.channel.type === 'dm') return;
+  let permlvl = 0;
+  let mod_role = message.guild.roles.find('name', settings.modrolename);
+  if (mod_role && message.member.roles.has(mod_role.id)) permlvl = 1;
+  let admin_role = message.guild.roles.find('name', settings.adminrolename);
+  if (admin_role && message.member.roles.has(admin_role.id)) permlvl = 2;
+  let manager_role = message.guild.roles.find('name', settings.managerrolename);
+  if (manager_role && message.member.roles.has(manager_role.id)) permlvl = 3;
+  let overlord_role = message.guild.roles.find('name', settings.overlordrolename)
+  if (overlord_role && message.member.roles.has(overlord_role.id)) permlvl = 4;
+  if (message.author.id === settings.ownerid) permlvl = 5;
+  return permlvl;
+};
+
+//ping log 
+//var regToken = /[\w\d]{24}\.[\w\d]{6}\.[\w\d-_]{27}/g;
+//client.on('debug', e => {
+//  console.log(e.replace(regToken, 'that was redacted'));
+//});
+
+
+client.login(settings.token);
---
+++ music.js
@@ -0,0 +1,78 @@
+const {Util } = require('discord.js');
+const settings = require('./settings.json');
+const YouTube = require('simple-youtube-api');
+const ytdl = require('ytdl-core-discord');
+
+const youtube = new YouTube(settings.GOOGLE_API_KEY);
+
+const queue = new Map();
+
+let loop = false;
+
+module.exports = {
+    serverQueue(guild) {
+        return queue.get(guild.id);
+    },
+    async handleVideo(video, message, voiceChannel, playlist = false) {
+        async function play(guild, song) {
+            const serverQueue = queue.get(guild.id);
+        
+            if (!song) {
+                serverQueue.voiceChannel.leave();
+                queue.delete(guild.id);
+                return;
+            }
+            //console.log(serverQueue.songs);
+        
+            const dispatcher = serverQueue.connection.playOpusStream(await ytdl(song.url))
+                .on('end', async reason => {
+                    if (reason === 'Stream is not generating quickly enough.') console.log('Song ended.');
+                    else console.log(reason);
+                    if (serverQueue.loop === true) await serverQueue.songs.push(song)
+                    serverQueue.songs.shift();
+                    play(guild, serverQueue.songs[0]);
+                })
+                .on('error', error => console.error(error));
+            dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+        
+            serverQueue.textChannel.send(`🎶 Start playing: **${song.title}**`);
+        }
+        const serverQueue = queue.get(message.guild.id);
+        //console.log(video);
+        const song = {
+            id: video.id,
+            title: Util.escapeMarkdown(video.title),
+            url: `https://www.youtube.com/watch?v=${video.id}`
+        };
+        if (!serverQueue) {
+            const queueConstruct = {
+                textChannel: message.channel,
+                voiceChannel: voiceChannel,
+                connection: null,
+                songs: [],
+                volume: 5,
+                playing: true,
+                loop: false
+            };
+            queue.set(message.guild.id, queueConstruct);
+    
+            queueConstruct.songs.push(song);
+    
+            try {
+                var connection = await voiceChannel.join();
+                queueConstruct.connection = connection;
+                play(message.guild, queueConstruct.songs[0]);
+            } catch (error) {
+                console.error(`I could not join the voice channel: ${error}`);
+                queue.delete(message.guild.id);
+                return message.channel.send(`I could not join the voice channel: ${error}`);
+            }
+        } else {
+            serverQueue.songs.push(song);
+            //console.log(serverQueue.songs);
+            if (playlist) return undefined;
+            else return message.channel.send(`✅ **${song.title}** has been added to the queue!`);
+        }
+        return undefined;
+    }
+}
