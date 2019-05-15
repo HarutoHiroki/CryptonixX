1259146522b6d338ce40edef5c730281ebea57d7
HarutoHiroki <hiroaki.haruto@gmail.com>
2019-05-15 12:40:54 +0700

    eh fix new purge cuz errors

--- commands/purge.js
+++ commands/purge.js
@@ -2,38 +2,41 @@ exports.run = function(client, message, args) {
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("❌**Error:** You don't have the **Manage Messages** permission!");
   if(!args[0]) return message.reply('Usage: purge all|bots|user|images <amount>')
   if(!args[1]) return message.channel.send("You need to specify an amount");
-  if(parseInt(args[1]) = NaN) return message.channel.send("You need to specify a valid amount");
-  if(args[0] = 'all') {
+  if(parseInt(args[1]) == NaN) return message.channel.send("You need to specify a valid amount");
+  if(args[0] === 'all') {
     let messagecount = parseInt(args.join(' '));
     message.channel.fetchMessages({
       limit: args[1]
     }).then(messages => message.channel.bulkDelete(messagecount))
     .catch(e => {
-      if(e) return message.reply("Error: ", e)
+      if(e) return message.channel.send("Error: ", e)
     })
   }
-  if(args[0] = 'bots') {
+  else if(args[0] === 'bots') {
     message.channel.fetchMessages({
       limit: args[1]
     }).then(messages => {
       const userMessages = messages.filter(message => message.author.bot) 
       message.channel.bulkDelete(userMessages)
     }).catch(e => {
-      if(e) return message.reply("Error: ", e)
+      if(e) return message.channel.send("Error: ", e)
     })
   }
-  if(args[0] = 'user') {
+  else if(args[0] === 'user') {
     message.channel.fetchMessages({
       limit: args[1]
     }).then(messages => {
       const userMessages = messages.filter(message => !message.author.bot) 
       message.channel.bulkDelete(userMessages)
     }).catch(e => {
-      if(e) return message.reply("Error: ", e)
+      if(e) return message.channel.send("Error: ", e)
     })
   }
-  if(args[0] = 'image') {
+  else if(args[0] === 'image') {
     message.reply("Upcoming feature :wink:")
+  }
+  else {
+    message.reply('Usage: purge all|bots|user|images <amount>')
   }
 };
 
