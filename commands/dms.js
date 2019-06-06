0dc3d3d9fdeeee6a5e3819854eecb634f3390d45
HarutoHiroki <hiroaki.haruto@gmail.com>
2019-06-06 12:41:39 +0700

    updated stuff

--- commands/dms.js
+++ commands/dms.js
@@ -4,24 +4,24 @@ const customisation = require('../customisation.json');
 exports.run = (client, message, args) => {
   message.delete();
     let msg = args.slice(1).join(' ');
-    let user = message.mentions.users.first();
-    if (message.mentions.users.size < 1) return message.reply('You must mention someone for me to dm them.');
-    if (!message.author.id === '242263403001937920') return message.reply('You do not have the permission to use this command!');
-    if (message.mentions.users.first().id === message.author.id) return message.reply('You can\'t DM yourself:facepalm:... Well you can, but WHY? ("Why not?" isn\'t accepted XD)');
+    let user = message.mentions.users.first() || message.guild.members.get(args[0])
+    if(!user) return message.reply('You must mention someone or provide a valid UserID for me to dm them.');
+    //if (!message.author.id === '242263403001937920') return message.reply('You do not have the permission to use this command!');
+    if (user.id === message.author.id) return message.reply('You can\'t DM yourself:facepalm:... Well you can, but WHY? ("Why not?" isn\'t accepted XD)');
     if (msg.length < 1) msg = 'Blank message. . .';
 
     const embed = new Discord.RichEmbed()
     .setColor(Math.floor(Math.random()*16777215))
     .setTimestamp()
-    .setTitle("You have a new message")
-    .addField('Remember:', `${user.username}, do not reply because ${message.author.username} will not recieve the reply`)
+    .setTitle(`You have a new message from ${message.author.username}!`)
+    .addField('Remember:', `Do not reply to me because ${message.author.username} will not recieve the reply, take your stuff to their dms instead :)`)
     .addField(`${message.author.username}'s message:`, msg)
     .setFooter(`© Cryptonix X Mod Bot by ${customisation.ownername}`);
-    message.mentions.users.first().send({embed}).catch(e =>{
+    user.send({embed}).catch(e =>{
       if (e) {
         return message.channel.send("That user unfortunately locked their DMs")
       }else{
-        message.channel.send("Successfully sent your message").then(message => {message.delete(1000)})
+        message.channel.send("Successfully sent your message").then(message => {message.delete(1500)})
       }
     });
 }
