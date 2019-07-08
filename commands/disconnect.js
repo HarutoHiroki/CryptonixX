33caa7cababb0aac2dd080e110719113d4165688
HarutoHiroki <hiroaki.haruto@gmail.com>
2019-07-08 10:46:21 +0700

    fixing disconnect command

--- commands/disconnect.js
+++ commands/disconnect.js
@@ -2,17 +2,19 @@ const Discord = require("discord.js");
 const ffmpeg = require("ffmpeg-binaries");
 const opusscript = require("opusscript");
 const customisation = require('../customisation.json');
+const music = require('../music')
 
 exports.run = (client, message, args) => {
-      if (message.member.voiceChannel) {
-        message.member.voiceChannel.leave()
+  const serverQueue = music.serverQueue(message.guild)
+      if (message.member.voiceChannel = serverQueue.voiceChannel) {
+        serverQueue.voiceChannel.leave();
         let embed = new Discord.RichEmbed()
         .setDescription('Disconnected.')
         .setColor('#ff9900')
         .setFooter(`© Cryptonix X Mod Bot by ${customisation.ownername}`);
       message.channel.send(embed);
     }else{
-        message.channel.send("You are not in a voice channel!")
+        message.channel.send("You are not in my voice channel!")
     }
   };
 exports.conf = {
